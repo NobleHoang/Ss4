4c81a6ea60e3c1492e829d21d3acac1948e6364e
NobleHoang <98103315+NobleHoang@users.noreply.github.com>
2022-04-20 13:06:47 +0700

    update

---
+++ src/TestCustomer.java
@@ -0,0 +1,10 @@
+public class TestCustomer {
+    public static void main(String[] args) {
+        Customer objCustomer = new Customer();
+        objCustomer.customerID=2109027;
+        objCustomer.customerName="Hieu";
+        objCustomer.customerAddress="Ha Noi - Viet Nam";
+        objCustomer.customerAge=20;
+        objCustomer.displayCustomerInformation();
+    }
+}
